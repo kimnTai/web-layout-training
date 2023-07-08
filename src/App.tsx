e77f59b90512275bc0fa9e54c36ff2dd7e65b958
kimn <a9523023650@gmail.com>
2023-07-08 20:04:29 +0800

    [新增] day4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import DayDefault from "./pages/day/Day3";
+import DayDefault from "./pages/day/Day4";
 import Day1 from "./pages/day/Day1";
 import Day2 from "./pages/day/Day2";
+import Day3 from "./pages/day/Day3";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<DayDefault />} />
       <Route path="/day1" element={<Day1 />} />
       <Route path="/day2" element={<Day2 />} />
+      <Route path="/day3" element={<Day3 />} />
     </Routes>
   );
 }
