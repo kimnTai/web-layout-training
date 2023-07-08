eade63120d183073e69bf089430a4ef803492daa
kimn <a9523023650@gmail.com>
2023-07-09 04:15:18 +0800

    [新增] day7

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import DayDefault from "./pages/day/Day6";
+import DayDefault from "./pages/day/Day7";
 import Day1 from "./pages/day/Day1";
 import Day2 from "./pages/day/Day2";
 import Day3 from "./pages/day/Day3";
 import Day4 from "./pages/day/Day4";
 import Day5 from "./pages/day/Day5";
+import Day6 from "./pages/day/Day6";
+import Day7 from "./pages/day/Day7";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Route path="/day3" element={<Day3 />} />
       <Route path="/day4" element={<Day4 />} />
       <Route path="/day5" element={<Day5 />} />
+      <Route path="/day6" element={<Day6 />} />
+      <Route path="/day7" element={<Day7 />} />
     </Routes>
   );
 }
